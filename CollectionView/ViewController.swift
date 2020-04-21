c27f1d2af95789c804e60886c2143b75cfdfdda0
abdelrahman <abdulrahmansalah86@gmail.com>
2020-04-22 00:03:00 +0200

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by abdulrahman on 4/22/20.
+//  Copyright © 2020 abdulrahmanAbdou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
